b686f48aa7c2e148e06d77da69def7333aeda1c9
mgraham8 <mgraham8@uw.edu>
2019-10-19 19:30:14 -0700

    final

--- set.js
+++ set.js
@@ -1,15 +1,31 @@
+/**
+ * Michael Graham
+ * 10/19/19
+ * Section AL - Tal Wolman
+ * HW2
+ *
+ * This is the JavaScript that provides interactivity to set.html. So far, it only allows the
+ * user to toggle between menu view and game view
+ */
+
 "use strict";
 
 (function() {
   window.addEventListener("load", clickListen);
 
+  /**
+  * Listens for the user to click a button, then calls toggleView function
+  */
   function clickListen() {
     id("start-btn").addEventListener("click", toggleView);
     id("back-btn").addEventListener("click", toggleView);
   }
+
+  /**
+   * Toggles the ui between the main menue and the game
+   */
   function toggleView() {
-    alert("enter");
-    id("menue-view").classList.toggle("hidden");
+    id("menu-view").classList.toggle("hidden");
     id("game-view").classList.toggle("hidden");
   }
 
